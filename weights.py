14793fa1982ef9eee5bb5665c4a81b046e2fd90b
Cecilia Case <cecilia@ceciliacase.com>
2015-05-16 03:31:41 +0000

    Added Smoothed Weight to the table. It is updated on any change to the table. It can now be accessed by my charting code.

--- weights.py
+++ weights.py
@@ -20,11 +20,11 @@ c.execute('''CREATE TABLE IF NOT EXISTS weights (date text unique, dateSubmitted
 # TODO: Remove this code when file upload works
 # todaysDate = time.strftime("%Y-%m-%d")
 # for x in range(0,11):
-# 	if x<9:
-# 		dateVal = '0'+str(x+1)
-# 	else:
-# 		dateVal = str(x+1)
-# 	c.execute('''INSERT OR IGNORE INTO weights VALUES(?, ?, ?)''',(('2015-05-'+ dateVal),todaysDate,200.0))
+#   if x<9:
+#     dateVal = '0'+str(x+1)
+#   else:
+#     dateVal = str(x+1)
+#   c.execute('''INSERT OR IGNORE INTO weights VALUES(?, ?, ?)''',(('2015-05-'+ dateVal),todaysDate,200.0))
 
 # # Delete all rows
 #c.execute('''DELETE from weights''')
@@ -34,43 +34,44 @@ db.commit()
 # Render index page
 @app.route('/')
 def index():
-	c.execute('''SELECT weight FROM weights ORDER BY date desc limit 1''')
-	print('before fetchall')
-	latestweight = c.fetchall()
-	if not latestweight:
-		latestweight.append(0)
-		print 'list empty'
-		print latestweight
-	print(latestweight[0])
-	print('Send index to template')
-	return template.render(currentdate=time.strftime("%Y-%m-%d"),
-		latestweight = latestweight[0],
-		url_for=url_for)
+  c.execute('''SELECT weight FROM weights ORDER BY date desc limit 1''')
+  print('before fetchall')
+  latestweight = c.fetchall()
+  if not latestweight:
+    latestweight.append(0)
+    print 'list empty'
+    print latestweight
+  print(latestweight[0])
+  print('Send index to template')
+  # Return the most recent weight, and todays date to preload the add weight form
+  return template.render(currentdate=time.strftime("%Y-%m-%d"),
+    latestweight = latestweight[0],
+    url_for=url_for)
 
 # Process a file upload
 @app.route("/upload", methods=['GET', 'POST'])
 def upload_file():
   if request.method == 'POST':
-		todaysDate = time.strftime("%Y-%m-%d")
-		loadedData = request.get_array(field_name='file')
-		# Test for header row
-		# TODO: test this code, make sure it works
-		if loadedData[0][0].isdigit():
-			print "no header to delete"
-		else:
-			print "deleted a header row"
-			del loadedData[0]
+    todaysDate = time.strftime("%Y-%m-%d")
+    loadedData = request.get_array(field_name='file')
+    # Test for header row
+    # TODO: test this code, make sure it works
+    if loadedData[0][0].isdigit():
+      print "no header to delete"
+    else:
+      print "deleted a header row"
+      del loadedData[0]
 
-		for row in loadedData:
-			# TODO: Add an error check for the parsed date
-			row[0] = parser.parse(row[0]).date()
-			if row[1]:
-				c.execute('''UPDATE OR IGNORE weights SET date=?, dateSubmitted=?, weight=? WHERE date=?''',(row[0], todaysDate, row[1], row[0]))
-				c.execute('''INSERT OR IGNORE INTO weights (date,dateSubmitted,weight) VALUES (?, ?, ?)''',(row[0], todaysDate, row[1]))
-		db.commit()
-		print 'excel file data loaded'
-		# TODO: Figure out how to pass the updated table back to automatically refresh?
-		return jsonify({"result": request.get_array(field_name='file')})
+    for row in loadedData:
+      # TODO: Add an error check for the parsed date
+      row[0] = parser.parse(row[0]).date()
+      if row[1]:
+        c.execute('''UPDATE OR IGNORE weights SET date=?, dateSubmitted=?, weight=? WHERE date=?''',(row[0], todaysDate, row[1], row[0]))
+        c.execute('''INSERT OR IGNORE INTO weights (date,dateSubmitted,weight) VALUES (?, ?, ?)''',(row[0], todaysDate, row[1]))
+    db.commit()
+    print 'excel file data loaded'
+    # TODO: Figure out how to pass the updated table back to automatically refresh?
+    return jsonify({"result": request.get_array(field_name='file')})
   return '''
   <!doctype html>
   <title>Upload an excel file</title>
@@ -88,56 +89,60 @@ def download_file():
 # Process weight row functions
 @app.route('/weights', methods=['POST','GET'])
 def weights():
-	error = None
-	todaysDate = time.strftime("%Y-%m-%d")
-	if request.method == 'POST':
-		if 'deleterow' in request.form:
-			# Delete the row in the database
-			rowIdToDelete = request.form['deleterow']
-			print rowIdToDelete
-			c.execute('''DELETE FROM weights WHERE rowid=?''',(rowIdToDelete,))
-			# c.execute('''DELETE FROM weights WHERE rowid=11''')
-			db.commit()
-			print 'Row deleted in the table'
-			returnValue = jsonify(result=rowIdToDelete)
+  error = None
+  todaysDate = time.strftime("%Y-%m-%d")
+  if request.method == 'POST':
+    if 'deleterow' in request.form:
+      # Delete the row in the database
+      c.execute('''DELETE FROM weights WHERE rowid=?''',(request.form['deleterow'],))
+      db.commit()
+      print 'Row deleted in the table'
+      returnValue = jsonify(result=request.form['deleterow'])
 
-		elif 'editrow' in request.form:
-			# Edit the row in the database
-			weightdata = request.form['editrow'], request.form['date'], todaysDate,request.form['weight']
-			rowIdToEdit = request.form['editrow']
-			# Update the current record
-			c.execute('''UPDATE weights SET rowid=?, date=?, dateSubmitted=?, weight=? WHERE rowid=?''',(weightdata[0], weightdata[1], weightdata[2], weightdata[3], weightdata[0]))
-			db.commit()
-			print 'Row edited in the table'
-			returnValue = jsonify(result=weightdata)
+    elif 'editrow' in request.form:
+      # Edit the row in the database
+      c.execute('''UPDATE weights SET rowid=?, date=?, dateSubmitted=?, weight=? WHERE rowid=?''',(
+                      request.form['editrow'],
+                      request.form['date'],
+                      todaysDate,
+                      request.form['weight'],
+                      request.form['editrow']))
+      db.commit()
+      print 'Row edited in the table'
+      returnValue = jsonify(result=(request.form['editrow'],
+                      request.form['date'],
+                      todaysDate,
+                      request.form['weight'],
+                      request.form['editrow']))
 
-		elif 'addrow' in request.form:
-			# Add weight data row, or update it if the date selected already has a match in the database
-			weightdata = request.form['date'],todaysDate,request.form['weight']
+    elif 'addrow' in request.form:
+      print request.form
+      print todaysDate
+      # Try to update a weight data row if the data already exists, and if it doesn't exist, insert it
+      c.execute('''UPDATE OR IGNORE weights SET date=?, dateSubmitted=?, weight=? WHERE date=?''',(request.form['date'],todaysDate,request.form['weight'],request.form['date']))
+      c.execute('''INSERT OR IGNORE INTO weights (date,dateSubmitted,weight) VALUES (?, ?, ?)''',(request.form['date'],todaysDate,request.form['weight']))
+      db.commit()
+      c.execute('''SELECT rowid,* FROM weights WHERE date=?''',(request.form['date'],))
+      fetchEditedRow = c.fetchall() # TODO: Should this be fetchone()?
+      print 'Row added or updated on the table'
+      returnValue = jsonify(result=fetchEditedRow[0])
+      print fetchEditedRow
+    else:
+      # TODO: log an error?
+      pass
+  else:
+    returnValue = retrieveWeighttable()
+  return returnValue
 
-			# Try to update a row if the data already esists, and if it doesn't exist, insert it
-			c.execute('''UPDATE OR IGNORE weights SET date=?, dateSubmitted=?, weight=? WHERE date=?''',(weightdata[0], weightdata[1],weightdata[2],weightdata[0]))
-			c.execute('''INSERT OR IGNORE INTO weights (date,dateSubmitted,weight) VALUES (?, ?, ?)''',(weightdata[0],weightdata[1],weightdata[2]))
-			db.commit()
-			c.execute('''SELECT rowid,* FROM weights WHERE date=?''',(weightdata[0],))
-			fetchLastRow = c.fetchall()
-			print 'Row added to the table'
-			returnValue = jsonify(result=fetchLastRow[0])
-		else:
-			pass
-	else:
-		# Fetch the inital table to put into DataTables and the chart
-		c.execute('''SELECT rowid,* FROM weights''')
-		weights = c.fetchall()
-		print 'Inital table fetched'
-		return jsonify(weights=weights)
-	return returnValue
-
-
-
-
+def retrieveWeighttable():
+  # Fetch the inital table to put into DataTables and the chart
+  c.execute('''SELECT rowid,* FROM weights''')
+  weights = c.fetchall()
+  print 'Table retrieved'
+#   print weights
+  return jsonify(weights=weights)
 
 
 if __name__ == '__main__':
-	# app.run(debug=True, host='0.0.0.0', port=8080)
-	app.run(host='0.0.0.0', port=8080)
+  # app.run(debug=True, host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080)
